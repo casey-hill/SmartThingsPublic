87aac019bc22761af9c208e2cbaf1979c4a1e5c1
Mr Awesome <af21a70c@opayq.com>
2016-01-05 21:32:22 -0500

    Fixed a few bugs

--- devicetypes/clh41413/my-garage-door-example.src/my-garage-door-example.groovy
+++ devicetypes/clh41413/my-garage-door-example.src/my-garage-door-example.groovy
@@ -2,6 +2,9 @@
 metadata {
 	definition (name: "My Garage Door (example)", namespace: "clh41413", author: "Casey Hill") {
 		capability "Contact Sensor"
+        capability "Switch"
+        command "on"
+        command "off"
         command "poll"
 	}
 
@@ -15,10 +18,16 @@ metadata {
 			state("open", label:'${name}', icon:"st.contact.contact.open", backgroundColor:"#ffa81e")
 			state("closed", label:'${name}', icon:"st.contact.contact.closed", backgroundColor:"#79b821")
 		}
+        
+         standardTile("mySwitch1", "device.mySwitch1", width: 1, height: 1, canChangeIcon: true, canChangeBackground: true, defaultState: 0) {
+			state "default", label: 'Off', action: "switch.off", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
+            state "on", label: 'On', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821"
+			state "off", label: 'Off', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
+		}
        
 
 		main(["myContact1"])
-        details(["myContact1","refresh"])
+        details(["myContact1","mySwitch1","refresh"])
 }
 
 def poll(){
@@ -46,3 +55,13 @@ def myPoll() {
     zigbee.smartShield(text: "poll").format()
 }
 
+def on() {
+    zigbee.smartShield(text: "SwitchOn").format()
+    //log.debug "on was set"
+    //zigbee.smartShield(text: "zone2On").format()
+}
+
+def off() {
+	zigbee.smartShield(text: "SwitchOn").format()
+    //zigbee.smartShield(text: "zone2Off").format()
+}
